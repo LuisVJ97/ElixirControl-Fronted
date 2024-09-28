6ed377215ccebd6e468d0940ea6340694fefd7e7
LuisVJ97 <u201717523@upc.edu.pe>
2024-09-27 23:07:43 -0500

    init comit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+import NavbarElixirControl from "./public/component/navbar-elixir-control.component.vue";
+
+
+export default {
+  name: "app",
+  components: {NavbarElixirControl},
+
+
+  title: 'Elixir Control',
+
+  created() {
+    console.log('App created');
+  }
+}
+
+</script>
+
+<template>
+
+  <div style="margin-top: 8%; margin-left:320px">
+     <router-view />
+  </div>
+
+
+</template>
+
+
+<style scoped>
+
+</style>
